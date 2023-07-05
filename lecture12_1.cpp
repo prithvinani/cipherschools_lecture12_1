da046e5aaaa1cbc6c981a57763717dc8b5d474f4
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-07-05 22:34:39 +0530

    Add files via upload

---
+++ lecture12_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"the address of a is "<<&a<<endl;
+    cout<<"the address of b is "<<&b<<endl;
+    return 0;
+}
